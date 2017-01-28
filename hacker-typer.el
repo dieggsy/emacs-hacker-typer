49336290333e419b41b2f4607fbb0886e965a86e
therockmandolinist <diegoamundo@gmail.com>
2017-01-28 10:58:55 -0500

    Add commentary for MELPA.

--- hacker-typer.el
+++ hacker-typer.el
@@ -27,6 +27,11 @@
 
 ;;; Commentary:
 
+;; This package provides a customizable implementation of hackertyper.com in
+;; emacs for your amusement. It opens a buffer in which typing any letter
+;; inserts a piece of a specified or randomly chosen script. It can also pull
+;; up a picture of "hackerman" on command.
+
 ;;; Code:
 
 (require 'async)
